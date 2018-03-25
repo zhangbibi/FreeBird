a37e1a9ff3c762d2ddfe9e886e8d5bbd894e2b05
zhangyaping <916459426@qq.com>
2018-03-25 22:47:41 +0800

    commit

--- fb-module/src/main/java/com/freebird/security/WebSecurityConfig.java
+++ fb-module/src/main/java/com/freebird/security/WebSecurityConfig.java
@@ -1,48 +1,48 @@
 package com.freebird.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * Created by zhangyaping on 17/8/6.
  */
 //@Configuration
 //@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Value("${auth.username}")
-    private String username;
-
-    @Value("${auth.password}")
-    private String password;
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Value("${auth.username}")
+//    private String username;
+//
+//    @Value("${auth.password}")
+//    private String password;
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+////        http.authorizeRequests()
+////                .antMatchers("/login.html").permitAll()
+////                .anyRequest().authenticated()
+////                .and()
+////                .formLogin()
+////                .loginPage("/login.html")
+////                .permitAll()
+////                .and()
+////                .logout()
+////                .permitAll();
+//
 //        http.authorizeRequests()
-//                .antMatchers("/login.html").permitAll()
 //                .anyRequest().authenticated()
 //                .and()
-//                .formLogin()
-//                .loginPage("/login.html")
-//                .permitAll()
-//                .and()
-//                .logout()
-//                .permitAll();
-
-        http.authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin().and()
-                .httpBasic();
-    }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser(username).password(password).roles("USER");
-    }
-}
+//                .formLogin().and()
+//                .httpBasic();
+//    }
+//
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser(username).password(password).roles("USER");
+//    }
+//}
